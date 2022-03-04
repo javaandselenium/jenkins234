a65130bc0904b7bec35281821c6536be9909a8a3
QSP <QSP@QSP>
2022-03-04 17:59:14 +0530

    Hi

---
+++ src/test/java/demo/Test5.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test5 {
+	@Test
+	public void t5() {
+	Reporter.log("hello postman",true);	
+	}
+
+}
